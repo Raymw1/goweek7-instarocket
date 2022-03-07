d7d87f1f147a4be2160529120d244e3570d66fd2
Raymw1 <rayan.rmw@gmail.com>
2022-03-06 22:48:42 -0300

    Adding ESLint

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,7 +5,12 @@ const upload = require("multer")(uploadConfig);
 const controllers = require("./app/controllers");
 
 routes.get("/posts", controllers.PostController.index);
-routes.post("/posts", upload.single("image"), controllers.PostController.store);
+routes.post(
+  "/posts",
+  (req, res) => console.log(req),
+  upload.single("image"),
+  controllers.PostController.store
+);
 routes.post("/posts/:id/like", controllers.LikeController.store);
 
 module.exports = routes;
